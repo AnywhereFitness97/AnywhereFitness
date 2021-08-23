a89710fbb484b5cf677e156b5dcebf93bc0dc95b
jMisch44 <jennimisch@gmail.com>
2021-08-23 14:22:00 -0400

    made basic register form structure

--- front-end/src/components/forms/Register.jsx
+++ front-end/src/components/forms/Register.jsx
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
-	return <div></div>;
+  const [newMember, setNewMember] = setState({});
+
+  return (
+    <div>
+      <h1>Register Here</h1>
+      <form>
+        <label>
+          First Name
+          <input
+            type="text"
+            name="first_name"
+            value={first_name}
+            onChange={(e) => {
+              setFirstName(e.target.value);
+            }}
+          />
+        </label>
+        <label>
+          {" "}
+          Last Name
+          <input type="text" name="last_name" />
+        </label>
+        <label>
+          {" "}
+          Email
+          <input type="email" name="email" />
+        </label>
+        <label>
+          {" "}
+          Phone Number
+          <input type="text" name="phone_Number" />
+        </label>
+        <label>
+          {" "}
+          Address
+          <input type="text" name="address" />
+        </label>
+        <label>
+          Second Address
+          <input type="text" name="address_2" />
+        </label>
+        <label>
+          {" "}
+          Zip
+          <input type="text" name="zip" />
+        </label>
+        <label>
+          {" "}
+          City
+          <input name="city" />
+        </label>
+        <label>
+          {" "}
+          State
+          <input type="text" name="state" />
+        </label>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
