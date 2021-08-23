13d50992f217879f3e16620d0c5adf4434a7e8fc
jMisch44 <jennimisch@gmail.com>
2021-08-23 14:54:50 -0400

    deleted unnecessary form fields, added handleChange function, and formValues state

--- front-end/src/components/forms/Register.jsx
+++ front-end/src/components/forms/Register.jsx
@@ -1,7 +1,21 @@
 import React, { useState } from "react";
 
+const initialFormValues = {
+  first_name: "",
+  last_name: "",
+  email: "",
+  role: "user",
+  username: "",
+  password: "",
+};
+
 const Register = () => {
-  const [newMember, setNewMember] = setState({});
+  const [formValues, setFormValues] = setState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
 
   return (
     <div>
@@ -12,51 +26,51 @@ const Register = () => {
           <input
             type="text"
             name="first_name"
-            value={first_name}
-            onChange={(e) => {
-              setFirstName(e.target.value);
-            }}
+            value={formValues["first_name"]}
+            onChange={handleChange}
           />
         </label>
         <label>
-          {" "}
           Last Name
-          <input type="text" name="last_name" />
+          <input
+            type="text"
+            name="last_name"
+            value={formValues["last_name"]}
+            onChange={handleChange}
+          />
         </label>
         <label>
-          {" "}
           Email
-          <input type="email" name="email" />
+          <input
+            type="email"
+            name="email"
+            value={formValues.email}
+            onChange={handleChange}
+          />
+        </label>
+        <select name="role" value={formValues.role} onChange={handleChange}>
+          <option name="user">User</option>
+          <option name="instructor">Instructor</option>
+        </select>
+        <label>
+          Username
+          <input
+            type="text"
+            name="username"
+            value={formValues.username}
+            onChange={handleChange}
+          />
         </label>
         <label>
-          {" "}
-          Phone Number
-          <input type="text" name="phone_Number" />
+          Password
+          <input
+            type="password"
+            name="password"
+            value={formValues.password}
+            onChange={handleChange}
+          />
         </label>
-        <label>
-          {" "}
-          Address
-          <input type="text" name="address" />
-        </label>
-        <label>
-          Second Address
-          <input type="text" name="address_2" />
-        </label>
-        <label>
-          {" "}
-          Zip
-          <input type="text" name="zip" />
-        </label>
-        <label>
-          {" "}
-          City
-          <input name="city" />
-        </label>
-        <label>
-          {" "}
-          State
-          <input type="text" name="state" />
-        </label>
+        <button>Submit</button>
       </form>
     </div>
   );
