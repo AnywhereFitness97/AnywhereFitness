eff48b52c1cd440ff1320a66212032409d67f0f9
riqthedev <riqthedev@gmail.com>
2021-08-23 19:22:44 -0400

    Completed Login

--- front-end/src/components/forms/Login.jsx
+++ front-end/src/components/forms/Login.jsx
@@ -1,29 +1,73 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import * as yup from "yup"
+
 
 const Login = () => {
-//   const initialLogin = {
-//     username: "",
-//     password: "",
-//   };
 
-  const [userName, setUsername] = useState("");
-  const [passWord, setPassword] = useState("");
+	// Initial Values
+  const initialLogin = {
+    username: "",
+    password: "",
+  };
 
-  
+  const initialErrors = {
+    username: "",
+    password: "",
+  };
+////////////////
+
+///// State
+  const [user, setUser] = useState(initialLogin);
+  const [errors, setErrors] = useState(initialErrors);
+  const [disabled, setDisabled] = useState(true)
+
+/////////////
+const setLoginErrors = (name,value) => {
+	yup.reach(schema, name).validate(value)
+	.then(() => setErrors({...errors, [name]: ""}))
+	.catch(err => setErrors({...errors, [name]: err.errors[0]})) 
+}
+
+////// Event Handlers
+  const handleChange = (event) => {
+	  const {name,value} = event.target
+	  setUser({...user, [name]: value})
+	  setLoginErrors(name, value)
+  }
+
+  const handleSubmit = (event) => {
+	  event.preventDefault();
+	
+  }
+  ////////////
+
+  //// Validation
+const schema = yup.object().shape({
+	username: yup.string().required("Please enter username"),
+	password: yup.string().required("Please enter password"),
+	
+})
 
 
+useEffect(() => {
+	schema.isValid(user).then(valid => setDisabled(!valid))
+}, [user])
+/////////
 
   return (
     <div>
-      <form>
-        
+      <form onSubmit={event => handleSubmit(event)}>
+        <div style={{color: "red"}}>
+			<div>{errors.username}</div>
+			<div>{errors.password}</div>
+		</div>
 		<label>
           Username:
           <input 
 		  name="username" 
 		  type="text"  
-		  onChange={(event) => setUsername(event.target.value)} 
-		  value={userName} 
+		  onChange={(event) => handleChange(event)} 
+		  value={user.username} 
 		  placeholder="username"
 		  />
         </label>
@@ -34,13 +78,13 @@ const Login = () => {
           <input 
 		  name="password" 
 		  type="text" 
-		  onChange={(event) => setPassword(event.target.value)} 
-		  value={passWord}
+		  onChange={(event) =>  handleChange(event)} 
+		  value={user.password}
 		  placeholder="password"
 		  />
         </label>
 
-		<button>Login</button>
+		<button disabled={disabled}>Login</button>
       </form>
     </div>
   );
