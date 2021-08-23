2dd8fbc0438a1e725c403680c7d1befe2eaf53d4
Ogden-R <rebeccaogden97@gmail.com>
2021-08-23 13:15:23 -0500

    create class file began.

--- front-end/src/components/forms/CreateClass.jsx
+++ front-end/src/components/forms/CreateClass.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CreateClass = () => {
-	return <div></div>;
-};
+	const [name, setName] = useState("");
+	const [time, setTime] = useState("");
+	const [classDescription, setClassDescription] = useState("");
+	const [date, setDate] = useState("");
+	const [duration, setDuration] = useState("");
+	const [typeClass, setTypeClass] = useState("");
+	const [intensityLevel, setIntensityLevel] = useState("");
+	const [location, setLocation] = useState("");
+	const [numberOfAttendees, setNumberOfAttendees] = useState("");
 
-export default CreateClass;
+	return (
+		<div>
+			<h1>Create Class</h1>
+			<div className="classForm">
+				<form>
+					<div>
+						<label>Class Name</label>
+						<input
+							type="text"
+							value={name}
+							onChange={(e) => {
+								setName(e.target.value);
+							}}
+						/>
+					</div>
+					<div>
+						<label>Class Time</label>
+						<input
+							type="text"
+							value={time}
+							onChange={(e) => {
+								setTime(e.target.value);
+							}}
+						/>
+					</div>
+					<div>
+						<label>Class Name</label>
+						<input
+							type="text"
+							value={classDescription}
+							onChange={(e) => {
+								setClassDescription(e.target.value);
+							}}
+						/>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
+};
