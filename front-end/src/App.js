530e4d35441c9dd8947f6584b3b74d281e05a8c1
riqthedev <riqthedev@gmail.com>
2021-08-23 17:35:09 -0400

    Login page done

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import React from "react";
+import Login from "./components/forms/Login";
+
 
 function App() {
-	return <div className="AppContainer"></div>;
+
+
+
+	return <div className="AppContainer">
+		
+	</div>;
 }
 
 export default App;
--- front-end/src/components/forms/Login.jsx
+++ front-end/src/components/forms/Login.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-	return <div></div>;
+//   const initialLogin = {
+//     username: "",
+//     password: "",
+//   };
+
+  const [userName, setUsername] = useState("");
+  const [passWord, setPassword] = useState("");
+
+  
+
+
+
+  return (
+    <div>
+      <form>
+        
+		<label>
+          Username:
+          <input 
+		  name="username" 
+		  type="text"  
+		  onChange={(event) => setUsername(event.target.value)} 
+		  value={userName} 
+		  placeholder="username"
+		  />
+        </label>
+
+
+		<label>
+        Password:
+          <input 
+		  name="password" 
+		  type="text" 
+		  onChange={(event) => setPassword(event.target.value)} 
+		  value={passWord}
+		  placeholder="password"
+		  />
+        </label>
+
+		<button>Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
