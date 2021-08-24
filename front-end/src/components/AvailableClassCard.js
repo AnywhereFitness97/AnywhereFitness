e60aff2151b829b84a7e3bad433a13845b55774c
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-08-24 17:38:19 -0400

    added routing to available class focus page

--- front-end/src/components/AvailableClassCard.js
+++ front-end/src/components/AvailableClassCard.js
@@ -1,18 +1,31 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
-export const AvailableClassCard = ({ card }) => {
+export const AvailableClassCard = (props) => {
+  const { card } = props;
+  const { push } = useHistory();
+  const handleClick = (e) => {
+    push(`/client/${card.id}`);
+  };
   return (
-    <div>
-      <img
-        src="https://images.unsplash.com/photo-1576678927484-cc907957088c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
-        alt="Gym"
-      />
-      <h2>{card["class_name"]}</h2>
-      <h3>{card["class_type"]}</h3>
-      <p>Intensity: {card["class_intensity_level"]}</p>
-      <p>{card["class_date"]}</p>
-      <p>{card["class_time"]}</p>
-      <p>{card["class_duration"]}</p>
+    <div className="card-available-wrap">
+      <div className="m-3 card-available" onClick={handleClick}>
+        <div className="card-available-img-wrap">
+          <img
+            src="https://images.unsplash.com/photo-1576678927484-cc907957088c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
+            alt="Gym"
+            className="img-fluid card-available-img"
+          />
+        </div>
+        <h2>{card["class_name"]}</h2>
+        <h3>{card["class_type"]}</h3>
+        <p>Intensity: {card["class_intensity_level"]}</p>
+        <p>{card["class_date"]}</p>
+        <p>{card["class_time"]}</p>
+        <p>{card["class_duration"]}</p>
+      </div>
     </div>
   );
 };
+
+export default AvailableClassCard;
--- front-end/src/components/Client.js
+++ front-end/src/components/Client.js
@@ -1,14 +1,18 @@
 import React from "react";
 import dummyData from "../dummyData";
-import { AvailableClassCard } from "./AvailableClassCard";
+import AvailableClassCard from "./AvailableClassCard";
 
 function Client(props) {
   return (
-    <div>
-      {dummyData.map((card) => {
-        return <AvailableClassCard card={card} />;
-      })}
-    </div>
+    <section className="py-4">
+      <div className="container">
+        <div className="d-flex flex-wrap">
+          {dummyData.map((card) => (
+            <AvailableClassCard card={card} />
+          ))}
+        </div>
+      </div>
+    </section>
   );
 }
 
