7a49e27f95d8396aabee846a15d9ba8446f86a50
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-08-24 11:20:09 -0400

    built header

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
 import Register from "./components/forms/Register";
+import Login from "./components/forms/Login";
+import Header from "./components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="AppContainer">
-      <Register />
+      <Header />
+
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
     </div>
   );
 }
---
+++ front-end/src/components/Header.js
@@ -0,0 +1,45 @@
+import React, { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+
+function Header(props) {
+  const pathname = useLocation().pathname;
+  console.log(pathname);
+
+  return (
+    <nav className="navbar py-3 bg-dark navbar-dark navbar-expand-sm">
+      <div className="container navbar-container">
+        <a className="navbar-brand">Anywhere Fitness</a>
+
+        <button
+          className="navbar-toggler ms-auto"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbar-menu"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse " id="navbar-menu">
+          <ul className="navbar-nav ms-auto">
+            {pathname === "/" && (
+              <li className="nav-item m-auto">
+                <Link to="/register" className="text-decoration-none">
+                  <a className="nav-link">Register</a>
+                </Link>
+              </li>
+            )}
+
+            {pathname === "/register" && (
+              <li className="nav-item m-auto">
+                <Link to="/" className="text-decoration-none">
+                  <a className="nav-link">Already Registered? Login.</a>
+                </Link>
+              </li>
+            )}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
