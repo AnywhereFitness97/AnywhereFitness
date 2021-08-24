e3cc8315f9ac115b7da8b3f3d2f4925b1590370e
riqthedev <riqthedev@gmail.com>
2021-08-24 01:41:57 -0400

    Finished Login Styling

--- front-end/src/components/forms/Login.jsx
+++ front-end/src/components/forms/Login.jsx
@@ -1,91 +1,143 @@
 import React, { useState, useEffect } from "react";
-import * as yup from "yup"
+import * as yup from "yup";
+import { Button, Paper, Grid } from "@material-ui/core";
 
 
 const Login = () => {
-
-	// Initial Values
+  // Initial Values
   const initialLogin = {
     username: "",
     password: "",
+    role: "",
   };
 
   const initialErrors = {
     username: "",
     password: "",
+    role: "",
   };
-////////////////
+  ////////////////
 
-///// State
+  ///// State
   const [user, setUser] = useState(initialLogin);
   const [errors, setErrors] = useState(initialErrors);
-  const [disabled, setDisabled] = useState(true)
+  const [disabled, setDisabled] = useState(true);
 
-/////////////
-const setLoginErrors = (name,value) => {
-	yup.reach(schema, name).validate(value)
-	.then(() => setErrors({...errors, [name]: ""}))
-	.catch(err => setErrors({...errors, [name]: err.errors[0]})) 
-}
+  /////////////
+  
 
-////// Event Handlers
+
+  ///// Error Setting
+  const setLoginErrors = (name, value) => {
+    yup
+      .reach(schema, name)
+      .validate(value)
+      .then(() => setErrors({ ...errors, [name]: "" }))
+      .catch((err) => setErrors({ ...errors, [name]: err.errors[0] }));
+  };
+/////////
+
+
+  ////// Event Handlers
   const handleChange = (event) => {
-	  const {name,value} = event.target
-	  setUser({...user, [name]: value})
-	  setLoginErrors(name, value)
-  }
+    const { name, value } = event.target;
+    setUser({ ...user, [name]: value });
+    setLoginErrors(name, value);
+  };
 
   const handleSubmit = (event) => {
-	  event.preventDefault();
-	
-  }
+    event.preventDefault();
+  };
   ////////////
 
   //// Validation
-const schema = yup.object().shape({
-	username: yup.string().required("Please enter username"),
-	password: yup.string().required("Please enter password"),
-	
-})
+  const schema = yup.object().shape({
+    username: yup
+      .string()
+      .required("Please enter username")
+      .min(3, "Username must be at least 3 characters"),
+    password: yup
+      .string()
+      .required("Please enter password")
+      .min(3, "Password must be at least 3 characters"),
+    role: yup.string().required("Please select a role"),
+  });
 
+  useEffect(() => {
+    schema.isValid(user).then((valid) => setDisabled(!valid));
+  }, [user]);
+  /////////
 
-useEffect(() => {
-	schema.isValid(user).then(valid => setDisabled(!valid))
-}, [user])
-/////////
+  /////// CSS/ Styling
+  const paperstyle = {
+    padding: 10,
+    height: "60vh",
+    width: 500,
+    margin: "5% auto",
+	backgroundColor: "#e0e0e0"
+  };
 
+  const style = { margin: "5%", padding: "2%" };
+
+  /////////////
   return (
     <div>
-      <form onSubmit={event => handleSubmit(event)}>
-        <div style={{color: "red"}}>
-			<div>{errors.username}</div>
-			<div>{errors.password}</div>
-		</div>
-		<label>
-          Username:
-          <input 
-		  name="username" 
-		  type="text"  
-		  onChange={(event) => handleChange(event)} 
-		  value={user.username} 
-		  placeholder="username"
-		  />
-        </label>
+      <Paper elevation={10} style={paperstyle}>
+        <form style={style} onSubmit={(event) => handleSubmit(event)}>
+          <div style={{ color: "red" }}>
+            <div>{errors.username}</div>
+            <div>{errors.password}</div>
+            <div>{errors.role}</div>
+          </div>
+          <Grid
+            container
+            direction="column"
+            justifyContent="flex-start"
+            alignItems="center"
+          >
+            <h1 style={{color:"#3f51b5"}}>Login</h1>
 
+            <input
+              name="username"
+              type="text"
+              onChange={(event) => handleChange(event)}
+              value={user.username}
+              placeholder="Username"
+              style={style}
+		
+            />
 
-		<label>
-        Password:
-          <input 
-		  name="password" 
-		  type="text" 
-		  onChange={(event) =>  handleChange(event)} 
-		  value={user.password}
-		  placeholder="password"
-		  />
-        </label>
+            <input
+              name="password"
+              type="password"
+              onChange={(event) => handleChange(event)}
+              value={user.password}
+              placeholder="Password"
+              style={style}
+            />
 
-		<button disabled={disabled}>Login</button>
-      </form>
+            <select
+              style={style}
+              value={user.role}
+              name="role"
+              onChange={handleChange}
+            >
+              <option value="">Select Role</option>
+              <option value="2">Instructor</option>
+              <option value="3">User</option>
+            </select>
+
+            <Button
+              style={style}
+              variant="contained"
+              color="primary"
+              disabled={disabled}
+            >
+              Sign In
+            </Button>
+          </Grid>
+        </form>
+      </Paper>
     </div>
   );
 };
