3ac52ad33a1ce9491bb6a49393ad823efddbb873
jMisch44 <jennimisch@gmail.com>
2021-08-24 12:53:13 -0400

    login form

--- front-end/src/components/forms/Login.jsx
+++ front-end/src/components/forms/Login.jsx
@@ -1,13 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+const initialValues = {
+  username: "",
+  password: "",
+};
+const initialFormErrors = {
+  username: "",
+  password: "",
+};
+
 const Login = () => {
+  const [user, setUser] = useState({});
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
   return (
     <div>
       <h1>Login Form</h1>
-      <Link to="/client">
-        <button>Submit</button>
-      </Link>
+      <form>
+        <label>
+          Username
+          <input
+            type="text"
+            name="username"
+            value={formValues.username}
+            onChange={handleChange}
+          />
+        </label>
+        <div className="error">{formErrors.username}</div>
+        <label>
+          Password
+          <input
+            type="text"
+            name="password"
+            value={formValues.password}
+            onChange={handleChange}
+          />
+        </label>
+        <div className="error">{formErrors.password}</div>
+        <Link to="/client">
+          <button>Submit</button>
+        </Link>
+      </form>
     </div>
   );
 };
