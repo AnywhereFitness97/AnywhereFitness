00e375cf641b6a2017648fd6a59f8834835a36bc
riqthedev <riqthedev@gmail.com>
2021-08-24 19:50:48 -0400

    merge conflicts

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,38 +1,32 @@
 import "./App.css";
-import React from "react";
+import Register from "./components/forms/Register";
 import Login from "./components/forms/Login";
-<<<<<<< Updated upstream
-
-=======
 import Header from "./components/Header";
 import Client from "./components/Client";
 import Instructor from "./components/Instructor";
 import CreateClass from "./components/forms/CreateClass";
 import AvailableClassFocus from "./components/AvailableClassFocus";
-//
+import UpdateForm from "./components/forms/UpdateForm";
+import UpcomingClasses from "./components/UpcomingClasses";
 import { Route, Switch } from "react-router-dom";
->>>>>>> Stashed changes
 
 function App() {
+  return (
+    <div className="AppContainer">
+      <Header />
 
-<<<<<<< Updated upstream
-
-
-	return <div className="AppContainer">
-		
-	</div>;
-=======
       <Switch>
         <Route exact path="/" component={Login} />
         <Route path="/register" component={Register} />
         <Route exact path="/client" component={Client} />
-        <Route exact path="/client/:id" component={AvailableClassFocus} />
+        <Route exact path="/client/focus/:id" component={AvailableClassFocus} />
+        <Route exact path="/client/upcoming" component={UpcomingClasses} />
         <Route exact path="/instructor" component={Instructor} />
-        <Route path="/instructor/add" component={CreateClass} />
+        <Route exact path="/instructor/add" component={CreateClass} />
+        <Route exact path="/instructor/:id" component={UpdateForm} />
       </Switch>
     </div>
   );
->>>>>>> Stashed changes
 }
 
 export default App;
