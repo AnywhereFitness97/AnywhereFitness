91ef6a2866c79cbeb77628f099787e101eb7920e
jMisch44 <jennimisch@gmail.com>
2021-08-24 13:40:04 -0400

    validation on login page

--- front-end/src/components/forms/Login.jsx
+++ front-end/src/components/forms/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import * as yup from "yup";
 
 const initialValues = {
   username: "",
@@ -9,14 +10,27 @@ const initialFormErrors = {
   username: "",
   password: "",
 };
+const FormSchema = yup.object().shape({
+  username: yup.string().required("this field is required"),
+  password: yup.string().required("this field is required"),
+});
 
 const Login = () => {
   const [user, setUser] = useState({});
   const [formValues, setFormValues] = useState(initialValues);
   const [formErrors, setFormErrors] = useState(initialFormErrors);
 
+  const validate = (name, value) => {
+    yup
+      .reach(FormSchema, name)
+      .validate(value)
+      .then(() => setFormErrors({ ...formErrors, [name]: "" }))
+      .catch((err) => setFormErrors({ ...formErrors, [name]: err.errors[0] }));
+  };
+
   const handleChange = (e) => {
     const { name, value } = e.target;
+    validate(name, value);
     setFormValues({ ...formValues, [name]: value });
   };
 
